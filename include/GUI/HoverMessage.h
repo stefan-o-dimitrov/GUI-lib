e1f07cc00a647f7bbf3f72a50d582f9be3be0c77
shtgames <shtgamessts@gmail.com>
2016-02-26 23:39:08 +0200

    v1.2.8
    - Added copyright notice to source and header files.

--- include/GUI/HoverMessage.h
+++ include/GUI/HoverMessage.h
@@ -1,11 +1,36 @@
-#pragma once
+////////////////////////////////////////////////////////////
+//
+// GUI-lib - Graphical User Interface Library
+// Copyright (C) 2014-2016 ShtGames (shtgamessts@gmail.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
 
-#include <SFML/Graphics/Shader.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
+#ifndef GUI_HOVER_MESSAGE
+#define GUI_HOVER_MESSAGE
 
 #include <unordered_map>
 #include <vector>
 #include <memory>
+
+#include <SFML/Graphics/Shader.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 
 #include "ColoredText.h"
 #include "Internals.h"
@@ -59,3 +84,5 @@ namespace gui
 		static const unsigned char TEXT_BOX_X_SPACING, TEXT_BOX_Y_SPACING, FADE_ANIMATION_FPS;
 	};
 };
+
+#endif
